f8bbd6b985d21c02300090b289378ecbc74666ca
ABDUL REHMAN <mr.abdulrehman.ar@gmail.com>
2022-04-27 15:41:00 +0500

    javascript

---
+++ javascript practise/js/script.js
@@ -0,0 +1,61 @@
+console.log("Hello World");
+// use for sigle line comment
+/* use for multiline comment */
+
+// variables in javascripts
+// numbers in javascript
+var a = 15;
+console.log(a);
+var b = 10;
+var sum = a + b;
+console.log(sum);
+
+var n = 125.2698;
+var m = sum + n;
+console.log(m);
+
+// string in javascript
+var x = 'Hello, this is a string';
+console.log(x);
+
+// boolean in javascript
+
+var check = false;
+console.log(check);
+check = true;
+console.log(check);
+
+// arrays in javascript
+//indexing     0        1       2       3          4
+let names = ["Ali", "Hassan", "Yasir", "Zain", "Rehman"];
+console.log(names[4]);
+
+// objects in javascript
+
+let student = {
+    "Name": 'Ali',
+    "Roll No": 'CIT-005',
+    "Class": 'CIT',
+    "Age": 25,
+    "Subjects": ["Web", "Graphics", "Mobile Apps"],
+    "Active": true
+};
+
+console.log(student);
+
+// Operators in javascript
+// arithmatic operators => +,-,*,/,%
+
+let i = 25;
+let j = 15;
+let add = i + i;
+console.log("Add = "+ add);
+let sub = i - j;
+console.log("Sub = "+ sub);
+let multi = i * j;
+console.log("Multi = "+ multi);
+let div = i / j;
+console.log("Div = "+ div);
+let mod = i % j;
+console.log("Mod = "+ mod);
+
